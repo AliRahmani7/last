4270b9d1064d591b4c18984051b6154e201bd542
AliRahmani7 <ali.rahmani.dr@gmail.com>
2022-07-13 05:31:13 -0400

    zap

---
+++ zap/src/main/java/com/product/test/ZapApplication.java
@@ -0,0 +1,13 @@
+package com.product.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapApplication.class, args);
+	}
+
+}
